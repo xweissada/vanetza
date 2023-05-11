f3203f8ee6d1a3ce939c20b4cd1c72279fc8a690
Adam Weisser <adam.weisser@email.cz>
2023-05-11 20:29:08 +0200

    v2cat: save verification key even in full request

--- tools/v2cat/main.cpp
+++ tools/v2cat/main.cpp
@@ -183,12 +183,14 @@ int main(int argc, const char** argv)
       std::cerr << "Enrolment failed." << std::endl;
       return 1;
     }
+    cm.SaveVerificationKey("ECVerificationKey");
     cm.SaveEc("EnrolmentCredential");
 
     if (!cm.RequestAt()) {
       std::cerr << "Authorization failed failed." << std::endl;
       return 1;
     }
+    cm.SaveVerificationKey("ATVerificationKey");
     cm.SaveAt("AuthorizationTicket");
 
     return 0;
